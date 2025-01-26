84ba262566438fbc0a74b4472d21d2e5d5c724bb
0x4007 <gpg@pavlovcik.com>
2025-01-26 20:38:30 +0900

    chore: try all logs

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,20 @@ import { isIssueCommentEvent } from "./types/typeguards";
 export async function runPlugin(context: Context) {
   const { logger } = context;
 
+  console.dir('logger.debug("plugin is starting.");');
   logger.debug("plugin is starting.");
   if (!isIssueCommentEvent(context)) return;
+  console.dir('logger.debug("this event is an issue comment event.");');
   logger.debug("this event is an issue comment event.");
   const { body } = context.payload.comment;
   const configCommandPattern = /^\/config/i;
   if (configCommandPattern.exec(body)) {
+    console.dir('logger.debug("`/config` command detected.");');
     logger.debug("`/config` command detected.");
     await syncConfigs(context);
+    console.dir('return logger.ok("ran `syncConfigs` successfully!");');
     return logger.ok("ran `syncConfigs` successfully!");
   }
+  console.dir('return logger.error("Unexpected error!");');
   return logger.error("Unexpected error!");
 }
